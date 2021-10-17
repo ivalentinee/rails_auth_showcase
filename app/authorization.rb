e5b13a2da7da98469bf18ebb5dd9abe02da6735a
V.Ionov <valentine.emperor@gmail.com>
2021-10-17 20:36:44 +0400

    per-role action grouping for naive authorization

--- app/authorization.rb
+++ app/authorization.rb
@@ -1,17 +1,11 @@
 module Authorization
   class << self
     def can?(action, subject, object)
-      case action
-      when :worker_task_update
-        subject.role == "worker" && object.assignee_id == subject.id
-      when :manager_task_update
-        subject.role == "manager" && object.managed_by.member?(subject.id)
-      when :manager_task_delete
-        subject.role == "manager" && object.managed_by.member?(subject.id)
-      when :manager_task_assign
-        subject.role == "manager" && object[:worker].managed_by.member?(subject.id) && object[:tasks].all? {|task| task.managed_by.member?(subject.id)}
-      when :manager_task_merge
-        subject.role == "manager" && object.all? {|task| task.managed_by.member?(subject.id)}
+      case subject.role
+      when "manager"
+        manager_can?(action, subject, object)
+      when "worker"
+        worker_can?(action, subject, object)
       else
         false
       end
@@ -20,6 +14,32 @@ module Authorization
     def authorize!(action, subject, object)
       unless can?(action, subject, object)
         raise Authorization::UnauthorizedException
+      end
+    end
+
+    private
+
+    def manager_can?(action, subject, object)
+      case action
+      when :manager_task_update
+        object.managed_by.member?(subject.id)
+      when :manager_task_delete
+        object.managed_by.member?(subject.id)
+      when :manager_task_assign
+        object[:worker].managed_by.member?(subject.id) && object[:tasks].all? {|task| task.managed_by.member?(subject.id)}
+      when :manager_task_merge
+        object.all? {|task| task.managed_by.member?(subject.id)}
+      else
+        false
+      end
+    end
+
+    def worker_can?(action, subject, object)
+      case action
+      when :worker_task_update
+        subject.role == "worker" && object.assignee_id == subject.id
+      else
+        false
       end
     end
   end
