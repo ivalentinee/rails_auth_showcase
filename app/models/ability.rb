2f2bb68f42db935e88372e5a6b2dee8955c1e8e2
V.Ionov <valentine.emperor@gmail.com>
2021-10-19 10:57:38 +0400

    per-role action grouping for cancancan authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,29 +1,37 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     if user.role == "worker"
-      can :worker_task_update, :all, assignee: user
+      worker_abilities(user)
     end
 
     if user.role == "manager"
-      can :manager_task_update, :all do |task|
-        task.managed_by.member?(user.id)
-      end
+      manager_abilities(user)
+    end
+  end
 
-      can :manager_task_delete, :all do |task|
-        task.managed_by.member?(user.id)
-      end
+  private
 
-      can :manager_task_assign, :all do |object|
-        object[:worker].managed_by.member?(user.id) && object[:tasks].all? {|task| task.managed_by.member?(user.id)}
-      end
+  def worker_abilities(user)
+    can :worker_task_update, :all, assignee: user
+  end
 
-      can :manager_task_merge, :all do |object|
-        object.all? {|task| task.managed_by.member?(user.id)}
-      end
+  def manager_abilities(user)
+    can :manager_task_update, :all do |task|
+      task.managed_by.member?(user.id)
+    end
+
+    can :manager_task_delete, :all do |task|
+      task.managed_by.member?(user.id)
+    end
+
+    can :manager_task_assign, :all do |object|
+      object[:worker].managed_by.member?(user.id) && object[:tasks].all? {|task| task.managed_by.member?(user.id)}
+    end
+
+    can :manager_task_merge, :all do |tasks|
+      tasks.all? {|task| task.managed_by.member?(user.id)}
     end
   end
 end
